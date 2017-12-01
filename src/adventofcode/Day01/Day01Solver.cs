a4a8a28ecf8c63c2d8a89cb78f0e6c5ab31aa786
sma <sma@skbkontur.ru>
2017-12-01 17:07:23 +0500

    Day01

---
+++ src/adventofcode/Day01/Day01Solver.cs
@@ -0,0 +1,40 @@
+﻿namespace adventofcode.Day01
+{
+    public class Day01Solver
+    {
+        private int sum;
+        private readonly int indexOffset;
+        private string input;
+        
+        public Day01Solver(int indexOffset = 1)
+        {
+            this.indexOffset = indexOffset;
+        }
+
+        public string Solve(string input)
+        {
+            sum = 0;
+            this.input = input;
+            
+            for (var i = 0; i < input.Length; i++)
+            {
+                AddToSumIfDigitsEquals(i, GetIndexByOffset(i));
+            }
+            return sum.ToString();
+        }
+        
+        private void AddToSumIfDigitsEquals(int leftIndex, int rightIndex)
+        {
+            var left = input[leftIndex];
+            var right = input[rightIndex];
+            if (left == right)
+                sum += int.Parse(right.ToString());
+        }
+
+        private int GetIndexByOffset(int currentIndex)
+        {
+            return (currentIndex + indexOffset) % (input.Length);
+        }
+         
+    }
+}
